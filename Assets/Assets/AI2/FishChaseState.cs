e5b174264733a32e6bec0ed3c2e1ef9102191d72
paul <8904872+paulwrodriguez@users.noreply.github.com>
2024-08-17 22:55:21 -0600

    fixed look at function when chasing

--- Assets/Assets/AI2/FishChaseState.cs
+++ Assets/Assets/AI2/FishChaseState.cs
@@ -38,7 +38,7 @@ public class FishChaseState : BaseState<FishStateMachine.FishState>
 
 
         Vector3 nextStep = Vector3.MoveTowards(go.transform.position, target, step);
-        Vector3 nextRotation = Vector3.RotateTowards(go.transform.position, target, rotationStep, 0.0f);
+        Quaternion nextRotation = LookAt1(nextStep, go, 1f);
 
         // if next step is within the bounds of the container then take the step, otherwise do wait
 
@@ -48,13 +48,23 @@ public class FishChaseState : BaseState<FishStateMachine.FishState>
         if (volumneCollider.bounds.Contains(nextStep))
         {
             go.transform.position = nextStep;
-            go.transform.rotation = Quaternion.LookRotation(nextRotation);
+            go.transform.rotation = nextRotation;
         }
         else
         {
             Debug.Log("in chase, next step is not within the volumne");
         }
 
+    }
+
+    private Quaternion LookAt1(Vector3 wanderTarget, GameObject go, float rotationSpeed)
+    {
+        var target = wanderTarget - go.transform.position;
+
+        var rotation = Quaternion.LookRotation(target);
+
+        //go.transform.rotation = Quaternion.Slerp(go.transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        return Quaternion.Slerp(go.transform.rotation, rotation, rotationSpeed);
     }
 
     public override void ExistState(GameObject g)
