ae022f0a78ddfc9fbdfcfb06a7c8b2ee6dfe5eda
Paul Rodriguez <8904872+paulwrodriguez@users.noreply.github.com>
2024-08-31 14:57:44 -0600

    added spawn manager and area under water

---
+++ Assets/Assets/AI3/tuna/EnemyAttackState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyAttackState : EnemyBaseState
+{
+    public EnemyDetection enemyDetection;
+
+    public EnemyAttackState(GameObject enemy, Animator animator, EnemyDetection enemyDetection) : base(enemy, animator)
+    {
+        this.enemyDetection = enemyDetection;
+    }
+
+    public override void Update()
+    {
+        Debug.Log("attack");
+        if (enemyDetection.targetWithinAttackRange && enemyDetection.targetGO != null)
+        {
+            enemyDetection.targetGO.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Assets/AI3/tuna/SpawnManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public List<GameObject> spawnLocations;
+    protected List<GameObject> spawnedEnemies = new();
+    public int MaxSpawn = 10;
+    public int SpawnCoolDownInSeconds = 10;
+    public GameObject parent;
+    private Utilities.CountdownTimer SpawnCooldownTimer;
+
+    // what do i want the spawn manager to do?
+    // I want the spawn manager to every tick, randomly place a prefab in the world
+
+    void Start()
+    {
+        SpawnCooldownTimer = new Utilities.CountdownTimer(SpawnCoolDownInSeconds);
+        SpawnCooldownTimer.OnTimerStop += SpawnEnemy;
+        SpawnCooldownTimer.Start();
+        // when the cooldown when the cooldown timer is stoped then trigger a spawn event 
+    }
+
+    private void SpawnEnemy()
+    {
+        if (spawnedEnemies.Count < MaxSpawn)
+        {
+            var randomLocationIndex = UnityEngine.Random.Range(0, spawnLocations.Count);
+            var spawnTransform = spawnLocations[randomLocationIndex].transform;
+            var newCreature = Instantiate(enemyPrefab, spawnTransform.position, spawnTransform.rotation, parent.transform);
+            spawnedEnemies.Add(newCreature);
+        }
+
+        SpawnCooldownTimer.Reset(SpawnCoolDownInSeconds);
+        SpawnCooldownTimer.Start();
+    }
+
+    void Update()
+    {
+        SpawnCooldownTimer.Tick(Time.deltaTime);
+    }
+}
