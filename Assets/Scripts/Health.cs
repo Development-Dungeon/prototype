cc44891d9b58f6a929fecc29999d65ba058afd63
Paul Rodriguez <8904872+paulwrodriguez@users.noreply.github.com>
2024-09-02 18:07:35 -0600

    shorted health class

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,24 +10,11 @@ public class Health : MonoBehaviour
     public float _currentHealth;
     public static event Action<float> HealthPercentChangeEvent;
 
-    private void Awake()
-    {
-        _currentHealth = _maxHealth;
-    }
-    private void Start()
-    {
-        TriggerEvent();
-    }
-
-    public void SetMaxHealth(float maxHealth)
-    {
-        _maxHealth = maxHealth;
-    }
-
-    public void SetCurrentHealth(float currentHealth)
-    {
-        _currentHealth = currentHealth;
-    }
+    private void Awake() => _currentHealth = _maxHealth;
+    private void Start() => TriggerEvent();
+    public void SetMaxHealth(float maxHealth) => _maxHealth = maxHealth;
+    public void SetCurrentHealth(float currentHealth) => _currentHealth = currentHealth;
+    public bool IsDead() => _currentHealth <= 0;
 
     private void TriggerEvent()
     {  
@@ -45,6 +32,5 @@ public class Health : MonoBehaviour
     }
 
 
-    public bool IsDead() => _currentHealth <= 0;
 
 }
