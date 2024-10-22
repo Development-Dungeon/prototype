44dc67375cacd4caf181425163f69e7394f86193
Paul Rodriguez <8904872+paulwrodriguez@users.noreply.github.com>
2024-10-21 22:23:03 -0600

    added respawn manager when player dies

---
+++ Assets/Scripts/RespawnManger.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManger : MonoBehaviour
+{
+
+    public GameObject playerGo;
+    public Transform respawnLocation; 
+    private Health playerHealth;
+    private FirstPersonController playerController;
+
+
+    void Start()
+    {
+        playerHealth = playerGo.GetComponent<Health>();
+        playerHealth.HealthPercentChangeEvent+= healthUpdateEvent;
+
+    }
+
+    private void healthUpdateEvent(float percentRemaining)
+    {
+        if(percentRemaining <= 0)
+        {  
+            if(respawnLocation != null)
+            {
+                playerHealth.SetCurrentHealth(playerHealth._maxHealth);
+                playerGo.transform.position = respawnLocation.position;
+                playerGo.transform.rotation = respawnLocation.rotation;
+		    }
+
+		}
+
+
+    }
+}
