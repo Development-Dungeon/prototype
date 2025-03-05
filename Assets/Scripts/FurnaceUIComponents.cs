1cf8639f15ee08da7365eca8ff423ec8bf7b737f
Paul Rodriguez <8904872+paulwrodriguez@users.noreply.github.com>
2025-03-04 22:01:57 -0700

    added ui components

---
+++ Assets/Scripts/FurnaceUIComponents.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FurnaceUIComponents : MonoBehaviour
+{
+    // slowly add all the parts of the UI here so I don't have to search for them in the other script 
+    // everything can be dynamic
+    // would it be more smart to have a group where all the text would be aligned to?
+
+    public TMP_Text consumptionRateText; 
+    public TMP_Text heatProductionText;
+    public TMP_Text furnaceIsOnText;
+    public TMP_Text progressText;
+}
